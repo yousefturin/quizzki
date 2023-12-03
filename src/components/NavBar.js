330fd4db279eaa3399d62c03b3cd0530ecb4cea5
yousefturin <yusefturin@gmail.com>
2023-12-03 11:03:47 +0300

    UI,UX is enhance for navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,14 +12,14 @@ function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
   const { logout, currentUser } = useAuth();
-  const [avatarSVG, setAvatarSVG] = useState(''); // State to store the user's image URL
+  const [avatarSVG, setAvatarSVG] = useState(""); // State to store the user's image URL
   const [error, setError] = useState("");
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
   const navigate = useNavigate();
+  const [extendedContainer, setExtendedContainer] = useState(false);
+  const [showBlock, setShowBlock] = useState(false);
   // This shit was pain in the ass giving me that a users is already a null so had to do that
-  const { displayName, email, photoURL } = currentUser || {};
-  
+  const { displayName, email, photoURL, uid  } = currentUser || {};
+
   const showButton = () => {
     if (window.innerWidth <= 960) {
       setButton(false);
@@ -33,34 +33,31 @@ function Navbar() {
     showButton();
   }, []);
 
-
   useEffect(() => {
     const fetchAvatar = async () => {
       try {
         // Fetch the SVG content from the API
-        const response = await fetch(`https://api.dicebear.com/7.x/avataaars/svg?seed=${photoURL}`);
+        const response = await fetch(
+          `https://api.dicebear.com/7.x/avataaars/svg?seed=${photoURL}`
+        );
 
         if (!response.ok) {
-          throw new Error('Failed to fetch avatar');
+          throw new Error("Failed to fetch avatar");
         }
 
         // Get the SVG content as text
         const svgContent = await response.text();
 
-        // Add width and height attributes to the SVG content
-      
-
         // Set the SVG content in the state
         setAvatarSVG(svgContent);
       } catch (error) {
-        console.error('Error fetching avatar:', error);
+        console.error("Error fetching avatar:", error);
       }
     };
 
     // Call the fetchAvatar function when the component mounts
     fetchAvatar();
   }, [photoURL]);
-
 
   window.addEventListener("resize", showButton);
 
@@ -75,10 +72,36 @@ function Navbar() {
     }
   }
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const handleContainer = () => setExtendedContainer(!extendedContainer);
+
+  // when closing the menu with button the container will be closed 
+  const handleContainerClick =()=>{
+    setExtendedContainer(false);
+    handleClick()
+  }
+  // when logging out the menu will close then the logo out will be handled
   const handleLogoutAndCloseMenu = () => {
     closeMobileMenu();
     handleLogout();
   };
+
+  // when cancel and open the Menu back the container will be closed.
+  const handleContainerCloseMobileMenu =()=>{
+    closeMobileMenu();
+    setShowBlock(false);
+  }
+
+
+  useEffect(() => {
+    if (currentUser !== null && extendedContainer) {
+      setShowBlock(true);
+    } else {
+      setShowBlock(false);
+    }
+  }, [currentUser, extendedContainer]);
+
 
   return (
     <>
@@ -88,29 +111,75 @@ function Navbar() {
             Quizzki
             <AppIcon />
           </Link>
-          <div className="menu-icon" onClick={handleClick}>
+          <div className="menu-icon" onClick={handleContainerClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             {currentUser && (
-              <li className="nav-user-info-item-container-wrapper">
-                <Link className="nav-user-info-item-container">
+              <li
+                className={
+                  extendedContainer
+                    ? "nav-user-info-item-container-wrapper active"
+                    : "nav-user-info-item-container-wrapper"
+                }
+                style={{ transition: "all 1s ease" }}
+              >
+                <Link
+                  className="nav-user-info-item-container"
+                  onClick={handleContainer}
+                >
                   <div className="nav-user-image-display-container">
                     <div className="nav-user-image-holder">
-                    <img 
-                    src={`data:image/svg+xml;utf8,${encodeURIComponent(avatarSVG)}`} 
-                    alt="User Avatar" 
-                    className="image-size-controller" />
-
+                      <img
+                        src={`data:image/svg+xml;utf8,${encodeURIComponent(
+                          avatarSVG
+                        )}`}
+                        alt="User Avatar"
+                        className="image-size-controller"
+                      />
                     </div>
                   </div>
-                  <div className="nav-user-name-email-display-container-wrapper">
-                    <div className="nav-user-name-display-container">
-                      {displayName}
+                  <div
+                    className={
+                      extendedContainer
+                        ? "nav-user-name-email-display-container-wrapper active"
+                        : "nav-user-name-email-display-container-wrapper"
+                    }
+                    style={{ transition: "all 1s ease" }}
+                  >
+                    <div>
+                      <div className="nav-user-name-display-container">
+                        {displayName}
+                      </div>
+                      <div className="nav-user-email-display-container">
+                        {email}
+                      </div>
                     </div>
-                    <div className="nav-user-email-display-container">
-                      {email}
-                    </div>
+                    {showBlock && (
+                      <>
+                        <hr className="horizontal-line-separator" />
+                    
+                      <li className="nav-item-user">
+                        <Link
+                          className="nav-links"
+                          to={`/users/${uid}`}
+                          onClick={handleContainerCloseMobileMenu}
+                        >
+                          Profile
+                        </Link>
+                      </li>
+                      </>
+                    )}
+                    {showBlock && (
+                      <li className="nav-item-user">
+                        <Link
+                          className="nav-links"
+                          onClick={handleLogoutAndCloseMenu}
+                        >
+                          Log out
+                        </Link>
+                      </li>
+                    )}
                   </div>
                 </Link>
               </li>
@@ -120,6 +189,15 @@ function Navbar() {
                 Home
               </Link>
             </li>
+            {/* <li className="nav-item">
+              <Link
+                to="/quiz-test"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Games
+              </Link>
+            </li> */}
             <li className="nav-item">
               <Link
                 to="/rank-hall"
@@ -129,6 +207,15 @@ function Navbar() {
                 Rank Hall
               </Link>
             </li>
+            {/* <li className="nav-item">
+              <Link
+                to="/information/how-to-play"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Game Rules
+              </Link>
+            </li> */}
             {currentUser ? null : (
               <li className="nav-item">
                 <Link
@@ -157,13 +244,6 @@ function Navbar() {
                   onClick={closeMobileMenu}
                 >
                   Log In
-                </Link>
-              </li>
-            )}
-            {currentUser !== null && (
-              <li className="nav-item">
-                <Link className="nav-links" onClick={handleLogoutAndCloseMenu}>
-                  Log out
                 </Link>
               </li>
             )}
