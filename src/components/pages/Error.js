49d440e31542bc5aa95cc0c81a2d399268ad0d4e
yousefturin <yusefturin@gmail.com>
2023-12-03 11:04:34 +0300

    error screen JSX added

---
+++ src/components/pages/Error.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Error.css";
+
+function Error() {
+  return (
+    <>
+      <section class="page-404">
+          <h1 class="page-404-text-header">404</h1>
+          <div class="page-404-background-bg"></div>
+          <div class="page-404-content-box">
+            <h3>Look like you're lost</h3>
+            <p>the page you are looking for not available!</p>
+            <div className="page-404-link-wrapper">
+              <Link to="/" class="link-404">
+                Go to Home
+              </Link>
+            </div>
+          </div>
+      </section>
+    </>
+  );
+}
+
+export default Error;
