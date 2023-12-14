c8e0dc78091a098c82c5118dba171525e09723a6
yousefturin <yusefturin@gmail.com>
2023-12-14 16:16:23 +0300

    params link is create with encryption, to be accessed as dynamic link

--- src/components/pages/QuizTest.js
+++ src/components/pages/QuizTest.js
@@ -3,19 +3,20 @@ import React, { useState, useEffect } from "react";
 // import questionsData from "../../db/db_easy_level.json";
 import { Card, Container, Button } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
-import { useQuiz  } from "../../contexts/QuizContext";
+import { useQuiz } from "../../contexts/QuizContext";
 import { usePageVisibility } from "react-page-visibility";
 import axios from "axios";
 import Footer from "../Footer";
 import "../App.css";
-import { Link, useParams } from 'react-router-dom';
-
+import { Link, useParams } from "react-router-dom";
+import CryptoJS from "crypto-js";
+import { useNavigation } from "../../contexts/NavigationContext";
 
 export default function QuizTest() {
-
   const { idTest } = useParams();
   const { selectedCard } = useQuiz();
   const [questionsData, setQuestionsData] = useState([]);
+  const { redirectPath, clearNavigation } = useNavigation();
 
   // dynamically importing the location of database for questions
   useEffect(() => {
@@ -84,30 +85,30 @@ export default function QuizTest() {
   const getRandomQuestion = () => {
     // Check if the questions are mounted
     if (questionsData.questions) {
-    // Check if all questions have been used
-    let availableQuestions = questionsData.questions.filter(
-      (question) => !usedQuestionIds.includes(question.id)
-    );
+      // Check if all questions have been used
+      let availableQuestions = questionsData.questions.filter(
+        (question) => !usedQuestionIds.includes(question.id)
+      );
 
-    // If all questions have been used, reset the used question IDs
-    if (availableQuestions.length === 0) {
-      setUsedQuestionIds([]);
-      availableQuestions = questionsData.questions; // Reset available questions
+      // If all questions have been used, reset the used question IDs
+      if (availableQuestions.length === 0) {
+        setUsedQuestionIds([]);
+        availableQuestions = questionsData.questions; // Reset available questions
+      }
+
+      // Select a random question from the available ones
+      const randomIndex = Math.floor(Math.random() * availableQuestions.length);
+      const randomQuestion = availableQuestions[randomIndex];
+
+      // Update state variables
+      setCurrentQuestion(randomQuestion);
+      setUserAnswer(null);
+      setIsCorrect(null);
+      setRemainingTime(30000000);
+
+      // Add the used question ID to the list    ***The number of added id is getting to more 6 zeros which may have effect on performance
+      setUsedQuestionIds((prevIds) => [...prevIds, randomQuestion.id]);
     }
-
-    // Select a random question from the available ones
-    const randomIndex = Math.floor(Math.random() * availableQuestions.length);
-    const randomQuestion = availableQuestions[randomIndex];
-
-    // Update state variables
-    setCurrentQuestion(randomQuestion);
-    setUserAnswer(null);
-    setIsCorrect(null);
-    setRemainingTime(30000000);
-
-    // Add the used question ID to the list    ***The number of added id is getting to more 6 zeros which may have effect on performance
-    setUsedQuestionIds((prevIds) => [...prevIds, randomQuestion.id]);
-  }
   };
   //#endregion
 
@@ -163,6 +164,7 @@ export default function QuizTest() {
   const handleStartButton = () => {
     setGameStarted(true);
     getRandomQuestion();
+    // clearNavigation();
   };
   //#endregion
 
@@ -189,6 +191,12 @@ export default function QuizTest() {
   useEffect(() => {
     console.log("Component mounted");
     getRandomQuestion();
+    const displayTimer = setInterval(() => {
+      clearNavigation();
+    }, 100000);
+    return () => {
+      clearInterval(displayTimer);
+    };
   }, []);
   //#endregion
 
@@ -197,6 +205,7 @@ export default function QuizTest() {
   useEffect(() => {
     if (gameOver) {
       storeUserData();
+      storeUserDataInURL(displayName, score, idTest);
     }
   }, [gameStarted, gameOver]);
 
@@ -204,25 +213,58 @@ export default function QuizTest() {
   const storeUserData = async () => {
     try {
       const currentTime = new Date().toLocaleString();
-      const apiUrl = "https://quizzkiapi-quizzki.onrender.com/v0/api/Quizzki/PostUserData";
-      const apiKey = "4111ad3f15115c1a4c60c3c0e73abdbbbffb435b"; 
-      const response = await axios.post(apiUrl, {
-        displayName,
-        time: currentTime,
-        score,
-        email
-      }, {
-        headers: {
-          "x-api-key": apiKey,
+      const apiUrl =
+        "https://quizzkiapi-quizzki.onrender.com/v0/api/Quizzki/PostUserData";
+      const apiKey = "4111ad3f15115c1a4c60c3c0e73abdbbbffb435b";
+      await axios.post(
+        apiUrl,
+        {
+          displayName,
+          time: currentTime,
+          score,
+          email,
         },
-      });
-  
-      console.log(response.data);
+        {
+          headers: {
+            "x-api-key": apiKey,
+          },
+        }
+      );
+
+      // console.log(response.data);
     } catch (error) {
       console.error("Error storing user data", error);
     }
   };
   //#endregion
+
+  // Encrypt function
+  const encryptData = (data) => {
+    const encrypted = CryptoJS.AES.encrypt(
+      data,
+      "6d846aa20bfa240f7000b4b616de5d77c30fb268"
+    );
+    return encrypted.toString();
+  };
+
+  // Function to store user data in URL parameters to be accessed as share score on gameOver events
+  const storeUserDataInURL = (displayName, score, idTest) => {
+    console.log(" this is inside the game quiz:idTest", idTest.toString());
+    // Encrypt data before constructing the URL
+    // Displayed Name is already string but incase there is no restrictions
+    // on username input it is converted to string, the score needs to be converted since CryptoJS only accept string format
+    const encryptedDisplayName = encryptData(displayName.toString());
+    const encryptedScore = encryptData(score.toString());
+    const encryptedIdTest = encryptData(idTest.toString());
+
+    const queryParams = `?displayName=${encodeURIComponent(
+      encryptedDisplayName
+    )}&score=${encodeURIComponent(encryptedScore)}&idTest=${encodeURIComponent(
+      encryptedIdTest
+    )}`;
+    const url = `/records/score/${queryParams}`;
+    console.log(url);
+  };
 
   if (gameOver) {
     return (
@@ -256,22 +298,24 @@ export default function QuizTest() {
                       Your final score is <strong>{score}</strong>.
                     </p>
                   </div>
+          
                   <Button onClick={restartGame} className="btn-form-submit">
                     Restart Game
                   </Button>
                   <Link to="/quiz-test" className="btn-mobile">
-                    <Button className="btn-form-submit-second"
-                    >
-                        Back to main
+                    <Button className="btn-form-submit-second">
+                      Back to main
                     </Button>
-                </Link>
+                  </Link>
                 </Card.Body>
                 {isCorrect === false && (
                   <>
                     <hr />
-                    <Card.Body className="card-body-question">
-                      <div className="question-wrapper">
-                        <h3 style={{fontSize:"1rem"}}>{currentQuestion.question}</h3>
+                    <Card.Body className="card-body-question-gameOver">
+                      <div className="question-wrapper-gameOver">
+                        <h3 style={{ fontSize: "1rem",color:"#4b4b4b" }}>
+                          {currentQuestion.question}
+                        </h3>
                       </div>
 
                       <ul className="questions-grid">
@@ -281,17 +325,18 @@ export default function QuizTest() {
                               className="btn-question-submit"
                               disabled={userAnswer !== null}
                               style={{
-                                marginTop:0,
+                                marginTop: 0,
                                 textAlign: "left",
                                 fontSize: "11px",
                                 background:
                                   option === currentQuestion.answer
-                                    ? "#663399"
-                                    : "gray",
+                                    ? "var( --successBg)"
+                                    : "#bfbcbc",
                                 color:
                                   option === currentQuestion.answer
                                     ? "white"
-                                    : "black",
+                                    : "#676666",
+                                cursor:"default",
                               }}
                             >
                               {option}
@@ -326,9 +371,17 @@ export default function QuizTest() {
                     <h1 style={{ alignSelf: "flex-start" }}>
                       Welcome! {displayName}{" "}
                     </h1>
-                    <h4 style={{ alignSelf: "flex-start" }}>
-                      are you ready to start the {idTest} game ?
-                    </h4>
+                    {redirectPath ? (
+                      <h4 style={{ alignSelf: "flex-start" }}>
+                        are you ready to compete and beat their score! in{" "}
+                        {idTest} game ?
+                      </h4>
+                    ) : (
+                      <h4 style={{ alignSelf: "flex-start" }}>
+                        are you ready to start the {idTest} game ?
+                      </h4>
+                    )}
+
                     <img
                       src="/images/image-start-1.svg"
                       alt="Rules illustration"
@@ -342,9 +395,12 @@ export default function QuizTest() {
                   >
                     Start Game
                   </Button>
-                  <Link to="/information/how-to-play" className="btn-link-secondary">
-                  Read Rules
-                </Link>
+                  <Link
+                    to="/information/how-to-play"
+                    className="btn-link-secondary"
+                  >
+                    Read Rules
+                  </Link>
                 </Card.Body>
               </Card>
             </div>
