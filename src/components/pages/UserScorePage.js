3888855e676a38f0767dfdbd565ebaa348bedd8b
yousefturin <yusefturin@gmail.com>
2023-12-21 17:59:54 +0300

    enhnaced the score page code

--- src/components/pages/UserScorePage.js
+++ src/components/pages/UserScorePage.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useMemo } from "react";
+import  { useEffect, useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import CryptoJS from "crypto-js";
-import { Card, Container, Button } from "react-bootstrap";
-import { useNavigation } from "../../contexts/NavigationContext";
-
+import { useRedirect } from "../../contexts/RedirectContext";
+import { generateIdHash } from "../utils/idHashGenerator"
 // Decrypt function
 const decryptData = (encryptedData) => {
     const bytes = CryptoJS.AES.decrypt(
@@ -17,8 +16,9 @@ const decryptData = (encryptedData) => {
 const UserScorePage = () => {
     const location = useLocation();
     const urlParams = useMemo(() => new URLSearchParams(location.search), [location.search]);
-    
-    const { setNavigation } = useNavigation();
+    const idHash = generateIdHash();
+
+    const { setRedirectedParams }= useRedirect();
     
     const navigate = useNavigate();
     // Decrypt data
@@ -51,62 +51,20 @@ const UserScorePage = () => {
         }
             // Log values for debugging
             console.log("urlParams:", urlParamNames);
+            setRedirectedParams(decryptedDisplayName, decryptedScore, decryptedIdTest);
+
+            
             // Cleanup function
             return () => {
                 clearInterval(decryptedDisplayName, decryptedScore, decryptedIdTest, urlParams);
         };
-    }, [decryptedDisplayName, decryptedScore, decryptedIdTest, navigate, urlParams]);
+    }, [decryptedDisplayName, decryptedScore, decryptedIdTest, navigate, urlParams, setRedirectedParams, idHash]);
+
+        return(
+            navigate(`/users/record/test/${idHash}`)
+        );
 
 
-    async function handleCompetition() {
-        // In normal way first navigation must be to log-in || sign-up but the app is wrapped in
-        // AuthProvider which does not allow user to access any page without login, so to navigate to needed URL is enough to trigger AuthProvider
-    try {
-        setNavigation(`/quiz-test/${decryptedIdTest}`);
-        navigate(`/quiz-test/${decryptedIdTest}`);
-    } catch (error) {
-        // Handle signup errors here
-        console.error("Signup error:", error);
-    }
-    }
-
-    return (
-        <div className="container-question-wrapper">
-            <Container
-                className="container-question-display"
-                style={{ minHeight: "92vh" }}
-            >
-                <div className="card-question-wrapper">
-                    <Card>
-                        <Card.Body>
-                            <div className="wrapper-start-container">
-                                <h1 style={{ alignSelf: "flex-start",textAlign:"left" ,fontWeight:"100" ,color:"#4A4A4A"}}>
-                                Hey, you! <br /> <strong style={{color:"var(--primaryDarkBg)"}} >{decryptedDisplayName.toLowerCase()} </strong> scored an impressive <strong style={{color:"var(--primaryDarkBg)"}} >{decryptedScore}</strong>
-                                    {" "}in the <strong style={{color:"var(--primaryDarkBg)"}} >{decryptedIdTest}</strong> category! 
-                                </h1>
-                                <h4 style={{ alignSelf: "flex-start",marginTop:"1.125rem" }}>
-                                What about a friendly
-                                    competition? Try to beat their score!
-                                </h4>
-                                <img
-                                    src="/images/image-winner-2.svg"
-                                    alt="Rules illustration"
-                                    className="image-winner-controller"
-                                />
-                            </div>
-
-                            <Button
-                                onClick={handleCompetition}
-                                className="btn-form-submit"
-                            >
-                                Start competition
-                            </Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </Container>
-        </div>
-    );
 };
 
 export default UserScorePage;
